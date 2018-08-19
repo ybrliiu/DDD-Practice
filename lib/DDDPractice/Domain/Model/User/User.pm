47f33acf8b545df317465cbf34e1203a3eb02244
mp0liiu <raian@reeshome.org>
2018-08-19 16:48:28 +0900

    create user model

---
+++ lib/DDDPractice/Domain/Model/User/User.pm
@@ -0,0 +1,28 @@
+package DDDPractice::Domain::Model::User::User {
+
+  use Mouse;
+  use DDDPractice::Exporter;
+
+  with 'DDDPractice::Domain::Role::Entity';
+
+  has id => (
+    is       => 'ro',
+    isa      => 'DDDPractice::Domain::Model::User::UserID',
+    required => 1,
+  );
+
+  has name => (
+    is       => 'rw',
+    isa      => 'DDDPractice::Domain::Model::User::FullName',
+    required => 1,
+  );
+
+  sub same_identity_as($self, $user) {
+    $self->id->same_value_as( $user->id );
+  }
+
+  __PACKAGE__->meta->make_immutable;
+
+}
+
+1;
