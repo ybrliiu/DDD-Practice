ff78d638239618b95af5931e188c6035727e080a
mp0liiu <raian@reeshome.org>
2018-10-31 02:53:02 +0900

    storable repository でトランザクション的なことをできるようにする

--- lib/DDDPractice/Infrastructure/Storable/User/UserRepository.pm
+++ lib/DDDPractice/Infrastructure/Storable/User/UserRepository.pm
@@ -2,29 +2,46 @@ package DDDPractice::Infrastructure::Storable::User::UserRepository {
 
   use Mouse;
   use DDDPractice::Exporter;
-  use Try::Tiny;
-  use Scalish qw( option right left );
-  use Storable qw( retrieve store );
   use namespace::autoclean;
+
+  use Try::Tiny;
+  use Fcntl qw( :flock );
+  use Scalish qw( option right left );
+  use Storable qw( retrieve nstore fd_retrieve );
 
   with 'DDDPractice::Domain::User::UserRepository';
 
   use constant FILE_NAME => './user.dat';
 
-  has memory => (
-    is      => 'ro',
-    isa     => 'HashRef[DDDPractice::Domain::User::User]',
-    builder => '_build_memory',
+  has is_locking => (
+    is       => 'rw',
+    isa      => 'Bool',
+    init_arg => undef,
+    default  => 0,
   );
 
-  sub _build_memory($self) {
+  has _maybe_memory => (
+    is       => 'rw',
+    isa      => 'Maybe[HashRef[DDDPractice::Domain::User::User]]',
+    init_arg => undef,
+    default  => undef,
+  );
+
+  has _maybe_fh => (
+    is       => 'rw',
+    isa      => 'Maybe[FileHandle]',
+    init_arg => undef,
+    default  => undef,
+  );
+
+  sub _read($self) {
     try {
-      retrieve( FILE_NAME );
+      retrieve $self->FILE_NAME;
     } catch {
       my $e = $_;
-      if ($e =~ /can't open (??{ FILE_NAME })/) {
-        store(+{}, FILE_NAME);
-        retrieve( FILE_NAME );
+      if ($e =~ /can't open (??{ $self->FILE_NAME })/) {
+        nstore(+{}, $self->FILE_NAME);
+        retrieve $self->FILE_NAME;
       }
       else {
         die $e;
@@ -33,24 +50,78 @@ package DDDPractice::Infrastructure::Storable::User::UserRepository {
   }
 
   sub find($self, $user_id) {
-    option $self->memory->{ $user_id->value };
+    my $memory = $self->_read;
+    option $memory->{ $user_id->value };
   }
 
   sub find_all($self) {
-    [ values $self->memory->%* ];
+    my $memory = $self->_read;
+    [ values $memory->%* ];
+  }
+
+  sub begin($self) {
+    $self->is_locking(1);
+    open my $fh, '+<', $self->FILE_NAME or die $!;
+    flock $fh, LOCK_EX or die $!;
+    $self->_maybe_fh($fh);
+    $self->_maybe_memory(fd_retrieve $fh);
+  }
+
+  sub commit($self) {
+    truncate $self->_maybe_fh, 0 or die $!;
+    seek $self->_maybe_fh, 0, 0 or die $!;
+    nstore_fd( $self->_maybe_memory, $self->_maybe_fh );
+    $self->_unlock;
+  }
+
+  sub rollback($self) {
+    $self->_unlock;
+  }
+
+  sub _unlock($self) {
+    $self->_maybe_fh->close;
+    $self->_maybe_memory(undef);
+    $self->_maybe_fh(undef);
+    $self->is_locking(0);
   }
 
   sub save($self, $user) {
-    $self->memory->{ $user->id->value } = $user;
-    store($self->memory, FILE_NAME)
+    my $is_success = do {
+      if ( $self->is_locking ) {
+        $self->_maybe_memory->{ $user->id->value } = $user;
+      }
+      else {
+        my $memory = $self->_read;
+        $memory->{ $user->id->value } = $user;
+        nstore $memory, $self->FILE_NAME;
+      }
+    };
+    $is_success
       ? right 'ユーザーデータの保存に成功'
       : left "ユーザーデータの保存に失敗 : $!";
   }
 
   sub remove($self, $user) {
-    delete $self->memory->{ $user->id->value }
+    my $is_success = do {
+      if ( $self->is_locking ) {
+        delete $self->_maybe_memory->{ $user->id->value }
+      }
+      else {
+        my $memory = $self->_read;
+        my $is_delete_success = delete $memory->{ $user->id->value };
+        if ( $is_delete_success ) {
+          nstore($memory, $self->FILE_NAME) or die $!;
+        }
+        $is_delete_success;
+      }
+    };
+    $is_success
       ? right 'ユーザーデーターの削除に成功'
-      : left 'ユーザーデーターの削除に失敗';
+      : left "ユーザーデータの保存に失敗 : $!";
+  }
+
+  sub DEMOLISH($self, @) {
+    $self->rollback if $self->is_locking;
   }
 
   __PACKAGE__->meta->make_immutable;
