20c904ffd6a039ba06d2d0fc769417df828eb08f
mp0liiu <raian@reeshome.org>
2018-10-31 14:47:36 +0900

    inmemory にトランザクションっぽいのを実装

--- lib/DDDPractice/Infrastructure/InMemory/User/UserRepository.pm
+++ lib/DDDPractice/Infrastructure/InMemory/User/UserRepository.pm
@@ -2,34 +2,80 @@ package DDDPractice::Infrastructure::InMemory::User::UserRepository {
 
   use Mouse;
   use DDDPractice::Exporter;
-  use Scalish qw( option right left );
   use namespace::autoclean;
+
+  use Scalish qw( option right left );
+  use Storable qw( dclone );
 
   with 'DDDPractice::Domain::User::UserRepository';
 
-  has memory => (
-    is      => 'ro',
-    isa     => 'HashRef[DDDPractice::Domain::User::User]',
-    default => sub { +{} },
+  my $Is_locking = 0;
+
+  # isa => 'HashRef[DDDPractice::Domain::User::User]'
+  my $Memory = +{};
+
+  has _is_transactioning => (
+    is       => 'rw',
+    isa      => 'Bool',
+    init_arg => undef,
+    default  => 0,
   );
 
+  has _be_operated_memory => (
+    is      => 'rw',
+    isa     => 'Maybe[HashRef[DDDPractice::Domain::User::User]]',
+    default => undef,
+  );
+
+  sub begin($self) {
+    return if $self->_is_transactioning;
+    if ( $Is_locking ) {
+      sleep 1;
+      $self->begin;
+    }
+    else {
+      $Is_locking = 1;
+      $self->_is_transactioning(1);
+      $self->_be_operated_memory( dclone $Memory );
+    }
+  }
+
+  sub commit($self) {
+    $Memory = $self->_be_operated_memory;
+    $self->_unlock;
+  }
+
+  sub rollback($self) {
+    $self->_unlock;
+  }
+
+  sub _unlock($self) {
+    $self->_be_operated_memory(undef);
+    $self->_is_transactioning(0);
+    $Is_locking = 0;
+  }
+
   sub find($self, $user_id) {
-    option $self->memory->{ $user_id->value };
+    option $Memory->{ $user_id->value };
   }
 
   sub find_all($self) {
-    [ values $self->memory->%* ];
+    [ values %$Memory ];
   }
 
   sub save($self, $user) {
-    $self->memory->{ $user->id->value } = $user;
+    $Memory->{ $user->id->value } = $user;
     right 'ユーザーデータの保存に成功'
   }
 
   sub remove($self, $user) {
-    delete $self->memory->{ $user->id->value }
+    delete $Memory->{ $user->id->value }
       ? right 'ユーザーデーターの削除に成功'
       : left 'ユーザーデーターの削除に失敗';
+  }
+
+  sub DEMOLISH($self, @) {
+    $self->rollback if $self->_is_transactioning;
   }
 
   __PACKAGE__->meta->make_immutable;
