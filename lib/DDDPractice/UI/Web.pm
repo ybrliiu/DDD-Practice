f7a568b195e446791550f9d025cd4d3f9d50bea1
mp0liiu <raian@reeshome.org>
2018-11-07 01:10:32 +0900

    use Plack::Middleware::Session

--- lib/DDDPractice/UI/Web.pm
+++ lib/DDDPractice/UI/Web.pm
@@ -3,9 +3,30 @@ package DDDPractice::UI::Web {
   use Mojo::Base 'Mojolicious';
   use DDDPractice::Exporter;
 
+  use Plack::Session;
+  use Plack::Session::Store::File;
+  use Plack::Session::State::Cookie;
+
+  use constant {
+    SESSION_EXPIRES_TIME   => 60 * 60 * 24,
+    SESSION_SEACRET_STRING => 'ddd_practice_seacret_string',
+    SESSION_KEY            => 'ddd_practice_sid',
+  };
+
   sub startup($self) {
 
     $self->plugin('Config');
+    $self->plugin(PlackMiddleware => [
+      Session => +{
+        state => Plack::Session::State::Cookie->new(
+          expiers     => SESSION_EXPIRES_TIME,
+          seacret     => SESSION_SEACRET_STRING,
+          # secure      => 1,
+          session_key => SESSION_KEY,
+        ),
+        store => Plack::Session::Store::File->new(dir => './etc/sessions'),
+      },
+    ]);
 
     my $r = $self->routes;
     $r->get( '/'             )->to('root#root');
